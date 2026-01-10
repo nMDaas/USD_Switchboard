599a3e679efcba88efd3c376c25073f70ab5662b
nMDaas <63910563+nMDaas@users.noreply.github.com>
2026-01-10 15:39:11 -0500

    Connected nodes together

--- 2A_tests.py
+++ 2A_tests.py
@@ -1,9 +1,13 @@
 import maya.cmds as cmds
+import maya.mel as mel
 
 graph_shape = cmds.createNode("bifrostGraphShape", name="myBifrostGraph")
 
 # Add a create_usd_stage node
-mel.eval(
-    f'vnnCompound "{graph_shape}" "/" '
-    f'-addNode "BifrostGraph,USD::Stage,create_usd_stage";'
-)
+cmds.vnnCompound(f"{graph_shape}","/", addNode="BifrostGraph,USD::Stage,create_usd_stage")
+
+# Add an add_to_stage node
+cmds.vnnCompound(f"{graph_shape}","/", addNode="BifrostGraph,USD::Stage,add_to_stage")
+
+#Connect create_usd_stage to add_to_stage node
+cmds.vnnConnect(f"{graph_shape}", "/create_usd_stage.stage", "/add_to_stage.stage")
